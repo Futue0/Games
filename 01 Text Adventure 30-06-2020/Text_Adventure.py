9f1c6c8b05a7c44072a42e682ac97efbddf1be2c
Futue0 <36641928+Futue0@users.noreply.github.com>
2020-07-01 15:26:40 +0100

    finished

--- 01 Text Adventure 30-06-2020/Text_Adventure.py
+++ 01 Text Adventure 30-06-2020/Text_Adventure.py
@@ -1,11 +1,104 @@
-from custom_functions import slow_text
+# Robin Vize 30-06-2020.
+# Basic text adventure, fantasy game.
+# Run from Python IDLE otherwise the delays don't work with Windows cmd.
+
+from custom_functions import *
 from time import sleep
 
-# Story introduction for the player
-slow_text("In the beginning there was a tiny elf.", 2)
-slow_text("This little cutey was called Squab.", 2)
-sleep(1)
-slow_text("He was very...")
-sleep(1)
-slow_text("EVIL!!!", 3)
+loop = True
 
+while loop:
+    # Story introduction for the player.
+    # Premise and some flavour.
+    slow_text("In the beginning there was a tiny elf.")
+    slow_text("This little cutey was called Squab.")
+    sleep(1)
+    slow_text("He was very...", 1)
+    sleep(1)
+    slow_text("EVIL!!!", 3)
+    print("")
+
+    # Player introduction and setup.
+    slow_text("Bartender: Hello traveller. How can I help you?")
+    slow_text("The Inn offers food and drink, a place to stay or a friendly chat.")
+
+    response_accepted = False
+    food_drink_keywords = ("food", "eat", "drink", "beer", "wine")
+    sleep_room_keywords = ("tired", "sleep", "room", "stay", "bed")
+    chat_keywords = ("chat", "talk", "listen")
+
+    # Get the players answer.
+    while not response_accepted:
+        response = input("You: ").lower()
+        
+        for word in food_drink_keywords:
+            if word in response:
+                response_chosen = "FOOD"
+                response_accepted = True
+                
+        for word in sleep_room_keywords:
+            if word in response:
+                response_chosen = "SLEEP"
+                response_accepted = True
+            
+        for word in chat_keywords:
+            if word in response:
+                response_chosen = "CHAT"
+                response_accepted = True
+
+        if not response_accepted:        
+            slow_text("Bartender: Sorry, can you rephrase that?", 1)
+
+    # Bartender responses.
+    if response_chosen == "FOOD":
+        food_response()
+    elif response_chosen == "SLEEP":
+        sleep_response()
+    else:
+        chat_response()
+
+    # Get players name.
+    class Player:
+
+        def __init__(self, name):
+            self.name = name
+
+    player_name = input("You: My name is ")
+
+    player = Player(player_name)
+
+    # Conflict appears.
+    slow_text("You've only just spoken your name when you hear a bang behind you.")
+    slow_text("Turning around you see a cute, little elf standing in the entrance.")
+    slow_text("Elf: Well, well, well, well. Of course you're here {}.".format(player.name))
+    slow_text("Elf: This is the part where I introduce myself before I kill you. It's Squab.")
+    slow_text("Squab: Meet you end {}!".format(player.name.upper()))
+    slow_text("The elf charges at you with a very large spoon (or maybe he's just far away?)")
+    slow_text("Do you want to fight or ignore him?")
+
+    # Player against Squab.
+    response_accepted = False
+
+    while not response_accepted:
+        response = input("You... ").lower()
+
+        if "fight" or "attack" or "stab" or "kill" in response:
+            second_response_chosen = "FIGHT"
+            response_accepted = True
+        elif "ignore" or "dodge" or "avoid" or "nothing" in response:
+            second_response_chosen = "IGNORE"
+            response_accetped = True
+
+        if not response_accepted:
+            slow_text('Try entering "fight" or "ignore".')
+
+    # Win or lose?
+    if second_response_chosen == "FIGHT":
+        fight_squab(response_chosen)
+    else:
+        ignore_squab()
+
+    # Try again or exit?
+    response = input('Type "quit" to exit or anything/enter to try again.')
+    if response.lower() == "quit":
+        loop = False
--- 01 Text Adventure 30-06-2020/custom_functions.py
+++ 01 Text Adventure 30-06-2020/custom_functions.py
@@ -3,7 +3,7 @@
 # The text is slowed equally across a total time so shorter texts
 # with the same total_time will type slower - i.e. can spend more
 # time per character than if the text was longer.
-def slow_text(text: str, total_time=1):
+def slow_text(text: str, total_time=2):
     from time import sleep
     from sys import exc_info
 
@@ -22,4 +22,52 @@ def slow_text(text: str, total_time=1):
     except:
         print("Unexpected error:", exc_info()[0])
         raise
+
+# Bartender response on player asking for food/drink.
+def food_response():
+    slow_text("Bartender: Okay, here's a plate of house special and glass of it too.")
+    slow_text("The bartender slides a plate and glass over. They certainly are special.")
+    slow_text("Bartender: What name am I putting on the tab?")
+
+# Bartender response on player asking to sleep/for a room.
+def sleep_response():
+    slow_text("Bartender: Single rooms are 1g per night. Seems appropriate seeing as you're alone.")
+    slow_text("The bartender places a rusty key on the bar and tells you it's the room at the end.")
+    slow_text("Bartender: What name are you booking in under?")
+
+# Bartender response on player asking to chat.
+def chat_response():
+    slow_text("Bartender: Praise Jesus, I haven't had a single customer all year and I'm lonely.")
+    slow_text("The bartender wipes a single tear from his eye. What a loser.")
+    slow_text("Bartender: Let's start with names shall we? Mine is Frimtickles.")
+
+# Fight Squab.
+def fight_squab(weapon):
+    if weapon == "FOOD":
+        weapon = "knife"
+        slow_text("Good luck that you have food. Even better luck that it was served with a knife and fork.")
+    elif weapon == "SLEEP":
+        weapon = "key"
+        slow_text("A room key versus a spoon? What sort of fight is this? Honestly...")
+    else:
+        slow_text("It's a shame you don't have anything to defend yourself with at hand.")
+        slow_text("Squab overpowers you with the spoon and takes both your eyes out their sockets.")
+        slow_text("It's probably for the best really. You don't want to see what he does next.")
+        slow_text("I hope you had life insurance because you're very much dead :(")
+        return
+        
+    slow_text("You steady yourself against the incoming attack.")
+    slow_text("This elf reminds you of a child, albeit a very angry one.")
+    slow_text("It's not much of a fight. Think you're a big man? Killing a child?")
+    slow_text("Regretfully you brandish your weapon, plunging your {} into the elf.".format(weapon))
+    slow_text("Yup, that will do it. He's dead, Mr Brave. I hope you're proud of yourself.")
+
+# Ignore Squab.
+def ignore_squab():
+    slow_text("You decide to ignore the person threatening to kill you.")
+    slow_text("This doesn't sound like a very good idea in my opinion.")
+    slow_text("You turn back around to face the bartender.")
+    slow_text("There's a sharp pain in your back. You look down to see the end of a spoon protruding from your lower abdomen")
+    slow_text("Told you it was a stupid idea.")
+    slow_text("It takes you only 4 seconds to collapse and a further 9 to leave this world entirely")
     
