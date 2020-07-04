8e158f608f947182349675a451934e2524948ca5
Futue0 <36641928+Futue0@users.noreply.github.com>
2020-07-04 23:02:53 +0100

    pyglet initial

--- 03 Single Screen Platform 2D 03-07-2020/Single_Screen_Platform_2D.py
+++
@@ -1,6 +0,0 @@
-# Robin Vize 03-07-2020.
-# 2D platformer with everything on a single screen.
-# The most basic 2D platformer possible and first game with pyglet.
-# Main menu, win condition, single screen.
-
-import pyglet
---
+++ 03 Single Screen Platform 2D 03-07-2020/version1/Single_Screen_Platform_2D.py
@@ -0,0 +1,21 @@
+# Robin Vize 03-07-2020.
+# 2D platformer with everything on a single screen.
+# The most basic 2D platformer possible and first game with pyglet.
+# Main menu, win condition, single screen.
+
+### Structure from Astraea example:
+###
+###     imports
+###     GLOBAL constants
+###
+###     global functions (used in objects below so called here)
+###     game objects (classes)
+###     UI objects (classes)
+###
+###     global functions (~mostly~ not used in objects)
+###     window setup + decorators
+###     resources setup
+###     global game state variables
+###     game update function
+###     pyglet.clock -> pyglet.app.run()
+
