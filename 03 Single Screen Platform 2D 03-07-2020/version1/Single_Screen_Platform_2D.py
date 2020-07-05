bdb5de51f253db99a94859bb44ddc111360bbf29
Futue0 <36641928+Futue0@users.noreply.github.com>
2020-07-05 22:22:49 +0100

    images redone

--- 03 Single Screen Platform 2D 03-07-2020/version1/Single_Screen_Platform_2D.py
+++ 03 Single Screen Platform 2D 03-07-2020/version1/Single_Screen_Platform_2D.py
@@ -19,3 +19,51 @@
 ###     game update function
 ###     pyglet.clock -> pyglet.app.run()
 
+import pyglet
+from pyglet.window import key
+
+
+# IMAGES.
+# Set path to correct folder.
+pyglet.resource.path = ['../assets']
+pyglet.resource.reindex()
+
+# Load image files.
+icon1 = pyglet.resource.image('16x16.png')
+icon2 = pyglet.resource.image('32x32.png')
+background_image = pyglet.resource.image('background.png')
+platform_image = pyglet.resource.image('platform.png')
+finish_image = pyglet.resource.image('finish.png')
+player_image = pyglet.resource.image('player.png')
+
+
+# WINDOW.
+# Game (and only) window.
+WINDOW_WIDTH, WINDOW_HEIGHT = 800, 800
+window = pyglet.window.Window(width=WINDOW_WIDTH, height=WINDOW_HEIGHT, caption='Single Screen Platformer')
+window.set_icon(icon1, icon2)
+
+quest_label = pyglet.text.Label('Reach the heart to win',
+                          font_name='Times New Roman',
+                          font_size=24,
+                          bold=True,
+                          color=(0, 0, 0, 255),
+                          x=window.width//2, y=window.height-50,
+                          anchor_x='center', anchor_y='center')
+
+@window.event
+def on_key_press(symbol, modifiers):
+    if symbol == key.A:
+        pass # move left
+    elif symbol == key.D:
+        pass # move right
+    elif symbol == key.SPACE:
+        pass # jump
+
+@window.event
+def on_draw():
+    window.clear()
+    background_image.blit(0, 0, width=window.width, height=window.height)
+    quest_label.draw()
+
+pyglet.app.run()
